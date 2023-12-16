fa735b0b730579130118d4e3e34c3d712ea178ea
alzvegbr <alzvegbr@itacyl.es>
2023-12-16 19:36:55 +0100

    estados2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
+
 const Header = (props) => {
-  console.log('Header: ' + props)
+  console.log('Header: ', props)
   return (
     <>
       <h1>{props.course}</h1>
@@ -8,51 +10,96 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
+  console.log('Part: ', props)
   return (
     <>
       <p>
-        {props.part} {props.exercises}
+        {props.part.name} {props.part.exercises}
       </p>
     </>
   )
 }
 
 const Content = (props) => {
+  console.log('Content: ', props.parts)
   return (
     <>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </>
   )
 }
 
 const Total = (props) => {
+  console.log('Total: ', props)
   return (
     <>
-      <p>Number of exercises {props.exercises}</p>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     </>
   )
 }
 
+const Hello = ({ name, age }) => {
+
+  const bornYear = () => new Date().getFullYear() - age;
+
+  return (
+    <div>
+      <p>
+        Hola {name}, tienes {age} años, por lo que tú probablemente naciste en {bornYear()}</p>
+    </div>
+  )
+}
+
+const Display = ({ counter }) => <div>Contador: {counter}</div>
+
+
+const Button = ({onSmash, text}) => <button onClick={onSmash}>{text}</button>
+
 const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+  const decreaseByOne = () => setCounter(counter -1)
+
+  const handleClick = () => {
+    console.log('clicked')
+  }
+  
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  const name = 'Peter'
+  const age = 49
+
+  console.log('rendering...', counter)
 
   return (
     <>
       <div>
         <Header course={course} />
-        <Content part1={part1} exercises1={exercises1}
-          part2={part2} exercises2={exercises2}
-          part3={part3} exercises3={exercises3} />
-        <Total exercises={exercises1 + exercises2 + exercises3} />
+        <Content parts={parts} />
+        <Total parts={parts} />
       </div>
+      <Hello name={name} age={age} />
+      <Display counter={counter}/>
+      <Button text="Añade al contador"  onSmash={increaseByOne}/>
+      <Button text="Resetea contador"  onSmash={setToZero}/>
+      <Button text="Resta al contador"  onSmash={decreaseByOne}/>
     </>
   )
 }
